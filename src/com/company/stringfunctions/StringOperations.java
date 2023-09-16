ca615363947a02ef7e978b31cf94bd36922b2926
rajnikant dankhara <rajnikant.dankhara@gmail.com>
2023-09-16 14:42:59 +0100

    experimented substring

--- src/com/company/stringfunctions/StringOperations.java
+++ src/com/company/stringfunctions/StringOperations.java
@@ -4,5 +4,8 @@ public class StringOperations {
 
     public static void main(String[] args) {
         System.out.println("Hello World".substring(3, 5));
+        System.out.println("Hello World".indexOf("World") >= 0);
     }
+
+
 }
